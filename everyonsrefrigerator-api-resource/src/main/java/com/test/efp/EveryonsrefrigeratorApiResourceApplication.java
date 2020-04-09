6f267e2fe05f95af229d0c75d2c2fece500f76ee
Songchanheum <39077239+Songchanheum@users.noreply.github.com>
2020-04-09 16:32:21 +0900

    init api
    2020.04.09 chsong init api server

---
+++ everyonsrefrigerator-api-resource/src/main/java/com/test/efp/EveryonsrefrigeratorApiResourceApplication.java
@@ -0,0 +1,13 @@
+package com.test.efp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryonsrefrigeratorApiResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryonsrefrigeratorApiResourceApplication.class, args);
+	}
+
+}
---
+++ everyonsrefrigerator-api-resource/src/test/java/com/test/efp/EveryonsrefrigeratorApiResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.efp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryonsrefrigeratorApiResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
