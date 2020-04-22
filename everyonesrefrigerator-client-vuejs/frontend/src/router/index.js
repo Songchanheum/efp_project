8553b5eabf5c2ae04fef12274f714e4eefccd017
chsong <chsong@thecnp.co.kr>
2020-04-22 15:26:33 +0900

    2020.04.22 chsong
    서버 구성을 위한 구조 변경

---
+++ everyonesrefrigerator-client-vuejs/frontend/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import CreatUser from '../views/auth/CreateUser.vue'
+import ChatLogin from '../views/chat/ChatLogin.vue'
+import ChatRoom from '../views/chat/ChatRoom.vue'
+
+Vue.use(VueRouter)
+
+const About = () => {
+  return import('../views/About.vue')
+}
+const Singin = () => {
+  return import('../views/auth/SignIn.vue')
+}
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => About()
+  },
+  {
+    path: '/sign',
+    name: 'SingIn',
+    component: () => Singin()
+  },
+  {
+    path: '/create',
+    name: 'CreateUser',
+    component: CreatUser
+  },
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: ChatLogin,
+    redirect: '/chat/login',
+    children: [
+      {
+        path: 'login',
+        component: ChatLogin,
+      },
+    ]
+  },
+  {
+    path: '/char-room/:username',
+    name: 'ChatRoom',
+    component: ChatRoom,
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  // base: process.env.BASE_URL,
+  routes
+})
+
+export default router
--- everyonesrefrigerator-client-vuejs/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  devServer: { host: '0.0.0.0', hot: true, disableHostCheck: true, },
-}
